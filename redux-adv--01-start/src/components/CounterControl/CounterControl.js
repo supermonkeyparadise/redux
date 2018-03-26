645b544ebbc8f385a7aeeed771d3cac70944400e
Unknown <steven.chou@uniqestate.com>
2018-03-26 14:01:21 +0800

    排版

--- redux-adv--01-start/src/components/CounterControl/CounterControl.js
+++ redux-adv--01-start/src/components/CounterControl/CounterControl.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './CounterControl.css';
 
-const counterControl = (props) => (
-    <div className="CounterControl" onClick={props.clicked}>
-        {props.label}
-    </div>
+const counterControl = props => (
+  <div className="CounterControl" onClick={props.clicked}>
+    {props.label}
+  </div>
 );
 
 export default counterControl;

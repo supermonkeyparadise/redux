fd76cd68bc9f8d74af5df842a16890d64e5a1abf
Unknown <steven.chou@uniqestate.com>
2018-03-26 13:19:28 +0800

    add basic middleware

--- redux-adv--01-start/src/index.js
+++ redux-adv--01-start/src/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import './index.css';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res: resultReducer
+  ctr: counterReducer,
+  res: resultReducer
 });
 
-const store = createStore(rootReducer);
+// [Middleware Basic] call by Redux
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    };
+  };
+};
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();

f578b0d966a28f33e2e682629cf035997f1e6edf
Unknown <steven.chou@uniqestate.com>
2018-03-27 10:47:19 +0800

    installing the Redux devtool

--- redux-burger--01-basic-setup/src/index.js
+++ redux-burger--01-basic-setup/src/index.js
@@ -10,7 +10,10 @@ import App from './App';
 import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>

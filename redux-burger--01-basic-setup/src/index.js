91f02e0d630ac01df85a05bd96830be950e3308f
Unknown <steven.chou@uniqestate.com>
2018-04-02 19:43:09 +0800

    Storing the Token

--- redux-burger--01-basic-setup/src/index.js
+++ redux-burger--01-basic-setup/src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 import App from './App';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -17,8 +18,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
-})
+  order: orderReducer,
+  auth: authReducer
+});
 
 const store = createStore(
   rootReducer,
--- redux-burger--01-basic-setup/src/store/actions/auth.js
+++ redux-burger--01-basic-setup/src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData
+    idToken: token,
+    userId
   };
 };
 
@@ -42,7 +43,7 @@ export const auth = (email, password, isSignup) => {
       .post(endpoint, authData)
       .then(response => {
         console.log(response.data);
-        dispatch(authSuccess(response.data));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(err => {
         // err 會由 firebase 提供完整的錯誤訊息

add3c95f6fc4c9ab29de9846bbcbc32e0f317550
Unknown <steven.chou@uniqestate.com>
2018-04-03 23:29:43 +0800

    Forwarding Unauthenticated Users

--- redux-burger--01-basic-setup/src/containers/Auth/Auth.js
+++ redux-burger--01-basic-setup/src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import classes from './Auth.css';
 
@@ -138,8 +139,12 @@ class Auth extends Component {
       errorMessage = <p>{this.props.error.message}</p>;
     }
 
+    // 登入後，自動導頁
+    let authRedirect = this.props.isAuthenticated ? <Redirect to="/" /> : null;
+    
     return (
       <div className={classes.Auth}>
+        {authRedirect}
         {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
@@ -156,7 +161,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
+    error: state.auth.error,
+    isAuthenticated: state.auth.token ? true : false
   };
 };
 

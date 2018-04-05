e4c6c92d67376d9856d2c68b482188911491e7db
Unknown <steven.chou@uniqestate.com>
2018-04-05 18:22:38 +0800

    Sharing the Validation Method

--- redux-burger--01-basic-setup/src/containers/Auth/Auth.js
+++ redux-burger--01-basic-setup/src/containers/Auth/Auth.js
@@ -8,6 +8,7 @@ import Input from './../../components/UI/Input/Input';
 import Button from './../../components/UI/Button/Button';
 import * as actions from './../../store/actions';
 import Spinner from './../../components/UI/Spinner/Spinner';
+import { checkValidity } from './../../shared/utility';
 
 class Auth extends Component {
   state = {
@@ -51,44 +52,13 @@ class Auth extends Component {
     }
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedControls = {
       ...this.state.controls,
       [inputIdentifier]: {
         ...this.state.controls[inputIdentifier],
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.controls[inputIdentifier].validation
         ),

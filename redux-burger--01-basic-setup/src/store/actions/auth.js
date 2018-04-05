64c648d452bc5e148299dc7df594ad931041d25a
Unknown <steven.chou@uniqestate.com>
2018-04-05 15:12:35 +0800

    Persistent Auth State with localStorage

--- redux-burger--01-basic-setup/src/store/actions/auth.js
+++ redux-burger--01-basic-setup/src/store/actions/auth.js
@@ -24,6 +24,10 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -56,7 +60,14 @@ export const auth = (email, password, isSignup) => {
     axios
       .post(endpoint, authData)
       .then(response => {
-        console.log(response.data);
+        // console.log(response.data);
+        const expirationDate = new Date(
+          new Date().getTime() + Number(response.data.expiresIn) * 1000
+        );
+        localStorage.setItem('token', response.data.localId);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
+
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         // 檢查登入是否過期
         dispatch(checkAuthTimeout(response.data.expiresIn));
@@ -73,5 +84,27 @@ export const setAuthRedirectPath = path => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate > new Date()) {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            expirationDate.getSeconds() - new Date().getSeconds()
+          )
+        );
+      } else {
+        dispatch(logout());
+      }
+    }
   };
 };
--- redux-burger--01-basic-setup/src/store/actions/index.js
+++ redux-burger--01-basic-setup/src/store/actions/index.js
@@ -9,4 +9,4 @@ export {
   purchaseInit,
   fetchOrders
 } from './order';
-export { auth, logout, setAuthRedirectPath } from './auth';
+export { auth, logout, setAuthRedirectPath, authCheckState } from './auth';

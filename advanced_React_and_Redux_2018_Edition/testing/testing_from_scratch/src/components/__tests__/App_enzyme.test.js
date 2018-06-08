acf159e55a9caae186f879ccb0accf558fdf318f
Unknown <steven.chou@uniqestate.com>
2018-06-08 17:31:46 +0800

    Code Reuse with BeforeEach

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App_enzyme.test.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App_enzyme.test.js
@@ -5,16 +5,17 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('shows a comment box(enzyme version)', () => {
-  const wrapped = shallow(<App />);
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
+it('shows a comment box(enzyme version)', () => {
   // 確認 App 裡，是否包含一個 CommentBox 元件
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment List(enzyme version)', () => {
-    const wrapped = shallow(<App />);
-
-    // 確認 App 裡，是否包含一個 CommentList 元件
-    expect(wrapped.find(CommentList).length).toEqual(1);
+  // 確認 App 裡，是否包含一個 CommentList 元件
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });

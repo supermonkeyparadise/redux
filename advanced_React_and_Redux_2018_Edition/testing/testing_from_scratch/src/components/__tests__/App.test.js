60fea28e90f69a1e9e2bb059a0586340c87d0bac
Unknown <steven.chou@uniqestate.com>
2018-06-08 14:47:54 +0800

    Verifying Component Existence

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App.test.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App.test.js
@@ -5,12 +5,14 @@ import App from './../App';
 
 it('shows a comment box', () => {
   // document 由 JSDOM 產生，模擬 browser
-  const div = document.create('div');
+  const div = document.createElement('div');
 
   ReactDOM.render(<App />, div);
 
   // Looks inside the div
   // and checks to see if the CommentBox is in there
+  //   console.log(div.innerHTML);
+  expect(div.innerHTML).toContain('Comment Box');
 
   // cleanup after our test run
   // remove div from document

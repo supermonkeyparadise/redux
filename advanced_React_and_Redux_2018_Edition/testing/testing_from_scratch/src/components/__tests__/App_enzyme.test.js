cfa10f7d9efa691974700bd96bc00c9870095c89
Unknown <steven.chou@uniqestate.com>
2018-06-08 17:01:59 +0800

    Expecting the Comment List

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App_enzyme.test.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/App_enzyme.test.js
@@ -3,10 +3,18 @@ import { shallow } from 'enzyme';
 
 import App from './../App';
 import CommentBox from './../CommentBox';
+import CommentList from './../CommentList'
 
 it('shows a comment box(enzyme version)', () => {
   const wrapped = shallow(<App />);
 
   // 確認 App 裡，是否包含一個 CommentBox 元件
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment List(enzyme version)', () => {
+    const wrapped = shallow(<App />);
+
+    // 確認 App 裡，是否包含一個 CommentList 元件
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });

6f124f564c93fd7493ba625df9f0354af1459ef3
Unknown <steven.chou@uniqestate.com>
2018-06-11 14:55:25 +0800

    The Provider Tag

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/index.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  // second argument will be our initial state for the redux store
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);

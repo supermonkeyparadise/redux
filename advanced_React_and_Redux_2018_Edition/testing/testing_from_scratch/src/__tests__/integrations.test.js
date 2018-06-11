dbeaaf5cf55e5c056488fa217baa841896078f92
Unknown <steven.chou@uniqestate.com>
2018-06-11 18:55:54 +0800

    Integration Tests in Action

---
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/__tests__/integrations.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Root from 'Root';
+import App from 'compenents/App';
+
+it('cat fetch a list of commemts and display them',()=>{
+    // Attempt to render the *entire* app
+
+    // find the 'fetchCommemts' button and click it
+
+    // Expect to find a list of commemts! 500 Li's
+})

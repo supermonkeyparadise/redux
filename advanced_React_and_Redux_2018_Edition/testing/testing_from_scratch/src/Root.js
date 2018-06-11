6ec0b7d49bf8f99f416620807a1a38607c341a0a
Unknown <steven.chou@uniqestate.com>
2018-06-11 18:24:33 +0800

    Fetching a Remote Resource

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/Root.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/Root.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 
 import reducers from 'reducers';
 
 // HOC
 export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
+  );
+
   return (
     // second argument will be our initial state for the redux store
-    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+    <Provider store={store}>{children}</Provider>
   );
 };

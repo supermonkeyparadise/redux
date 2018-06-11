2bfade449e88b35788ab44ec77dbdd76b740b248
Unknown <steven.chou@uniqestate.com>
2018-06-11 11:53:17 +0800

    Retrieving Prop Values

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/CommentBox.test.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/components/__tests__/CommentBox.test.js
@@ -22,3 +22,18 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+// 模擬事件
+it('has a text area that users can type in', () => {
+  // step 1: 模擬事件
+  wrapped.find('textarea').simulate('change', {
+    // 觸發事件後的 event
+    target: { value: 'new comment' }
+  });
+
+  // step 2: 更新元件
+  // forcing component update
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});

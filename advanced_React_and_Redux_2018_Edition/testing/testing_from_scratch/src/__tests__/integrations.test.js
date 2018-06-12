ae64c86b07d488fb6c05dbaf24d631fa33487160
Unknown <steven.chou@uniqestate.com>
2018-06-12 11:00:53 +0800

    Faking Requests with Moxios

--- advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/__tests__/integrations.test.js
+++ advanced_React_and_Redux_2018_Edition/testing/testing_from_scratch/src/__tests__/integrations.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 
 import Root from 'Root';
 import App from 'components/App';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [
+      { name: 'Fetched #1' },
+      { name: 'Fetched #2' },
+      { name: 'Fetched #3' }
+    ]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it('cat fetch a list of commemts and display them', () => {
   // Attempt to render the *entire* app
@@ -13,6 +30,8 @@ it('cat fetch a list of commemts and display them', () => {
   );
 
   // find the 'fetchCommemts' button and click it
+  wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to find a list of commemts! 500 Li's
+  expect(wrapped.find('li').length).toEqual(500);
 });
